09bbbb3e56e723c9c0b3c8b3e07b1ae6c5e4652f
krishnnavyas36 <krishnavyastr@gmail.com>
2023-12-13 05:04:00 -0800

    commit 2

--- pycontacts/urls.py
+++ pycontacts/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path('contacts/<int:pk>/', contact_detail, name='contact_detail'),
     path('contacts/create/', contact_create, name='contact_create'),
     path('contacts/<int:pk>/update/', contact_update, name='contact_update'),
-    path('contacts/<int:pk>/delete/', contact_confirm_delete, name='contact_confirm_delete'),
+    path('contacts/<int:pk>/delete/', contact_confirm_delete, name='contact_confirm_delete'),  # Use the correct name
 ]
+
 
 
 
