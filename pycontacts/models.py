6389b6e0a9068c6c591111efbc576ec7b71fd9d2
krishnnavyas36 <krishnavyastr@gmail.com>
2023-12-13 13:11:53 -0800

    cmit23

--- pycontacts/models.py
+++ pycontacts/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=100, uniqe = True)
+    email = models.EmailField(uniqe = True)
+    phone_number = models.CharField(max_length=20, uniqe = True)
 
     class Meta:
         app_label = 'pycontacts'
